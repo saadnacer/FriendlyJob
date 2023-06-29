498bce81eaba59184276b19e19b3a51bddf1cdb4
saadnacer <saadnacer@laposte.net>
2023-06-29 12:26:25 +0200

    restructuration du code la route accueil

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',function () { return view('welcome'); });
-Route::get('/accueil', function () { return view('accueil');
-});
+Route::get('/accueil', function () { return view('accueil'); });
 
 
 
