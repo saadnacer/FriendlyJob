f8be2557bbbbcb53a525c19369dc0fa7ae10565c
Ryane19 <djermouneryane@gmail.com>
2023-06-30 10:45:35 +0200

    ajout du model du jobworker

---
+++ app/Models/jobworker.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+// ajout du model(a peu pres comme une classe metier)du jobworker pour recuperer les données de la base de données
+// et pouvoir les manipuler dans notre environnement
+
+class jobworker extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nom',
+        'prenom',
+        'email',
+        'pwd',
+        'siret',
+        'activite',
+        'prix'
+    ];
+}
